365ffa0dadefc3277cc7177f07fcc1e75e935405
TopherBillu <nyashamangurenje16@gmail.com>
2022-09-18 13:41:33 -0700

    Fixed the slider

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  //   margin: 10,
-  nav: false,
-  autoplay: true,
-  dots: true,
-  autoplayTimeout: 4000,
-  smartSpeed: 1500,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    // 600: {
-    //   items: 2,
-    // },
-    // 1000: {
-    //   items: 4,
-    // },
-  },
-});
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   //   margin: 10,
+//   nav: false,
+//   autoplay: true,
+//   dots: true,
+//   autoplayTimeout: 4000,
+//   smartSpeed: 1500,
+//   autoplayHoverPause: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     // 600: {
+//     //   items: 2,
+//     // },
+//     // 1000: {
+//     //   items: 4,
+//     // },
+//   },
+// });
